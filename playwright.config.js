30ba78582da0c2805e7fba29e262d6de5bdda4b4
upeemia <alinader02@mail.ru>
2025-11-16 00:30:43 +0300

    fix

--- playwright.config.js
+++ playwright.config.js
@@ -29,8 +29,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['line'], ['html', { open: 'never' }], ['allure-playwright']],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+ /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
