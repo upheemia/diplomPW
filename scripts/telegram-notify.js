2a6cfa9a4b08a92f78bcf044d3fd95e7908c22a9
upeemia <alinader02@mail.ru>
2025-11-17 22:25:42 +0300

    fix import

--- scripts/telegram-notify.js
+++ scripts/telegram-notify.js
@@ -1,6 +1,9 @@
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
+// const https = require('https');
+// const fs = require('fs');
+// const path = require('path');
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
 
 class TelegramNotifier {
     constructor() {
